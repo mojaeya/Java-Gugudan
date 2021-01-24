5d04975afe5a37a3fcce2ad0e7c2379b54774f9d
ghwo5 <ghwo5@DESKTOP-GSK9HJS>
2021-01-25 00:16:41 +0900

    메소드 활용해 구구단 다시 구현

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,30 +1,36 @@
-import java.util.Scanner;
-
 public class Gugudan {
-
-	public static void main(String[] args) {
+	public static int[] calculate(int times) {
+		       //반환출력/ 메소드명        /입력//
 		int[] result = new int[9];
 		for(int i=0; i < result.length; i++) {        
-			result[i] = 2 * (i + 1);     //이중 for문 만들어보기
+			result[i] = times * (i + 1);    
 		}
+		return result; //반환 : return으로 표시
+	}
+	
+	public static void print(int[] result) {     //void = 메소드를 출력했을때 반환되는 값이 없다. 입력만 있을뿐.
 		for(int i=0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
+	}
+
+//	public static void main(String[] args) {
+//		int[] result = calculate(2);
+//		print(result);
+//		
+//		int[] times3 = calculate(3);
+//		print(times3);
+//		
+//		int[] times4 = calculate(4);
+//		print(times4);
+//		
+//		int[] times5 = calculate(5);
+//		print(times5);
 		
-		int[] times3 = new int[9];
-		for(int i=0; i < times3.length; i++) {
-			times3[i] = 3 * (i + 1);
-		}
-		for(int i=0; i < times3.length; i++) {
-			System.out.println(times3[i]);
-		}
-		
-		int[] times4 = new int[9];
-		for(int i=0; i < times4.length; i++) {
-			times4[i] = 4 * (i + 1);
-		}
-		for(int i=0; i < times4.length; i++) {
-			System.out.println(times4[i]);
+	public static void main(String[] args) {
+		for(int i = 2; i < 10; i++) {
+			int[] result = calculate(i);
+			print(result);
 		}
 	}
 }
